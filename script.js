20151cd7257fdf289cde51c3b4b98e1943da80af
Jauhien <ylipko@gravityglobal.com>
2023-05-11 12:59:23 +0200

    feat: Add popup section with js, refactor styles and add icons

---
+++ script.js
@@ -0,0 +1,37 @@
+const popup = document.querySelector('.popup');
+const popupForm = document.querySelector('.popup__form');
+const popupCloseBtn = popup.querySelector('.popup__close-button');
+const nameInput = document.querySelector('.popup__input[name="profile-name"]');
+const subtitleInput = document.querySelector('.popup__input[name="profile-subtitle"]');
+let name = document.querySelector('.profile__name');
+let subtitle = document.querySelector('.profile__subtitle');
+const editBtn = document.querySelector('.profile__edit-button');
+
+function getValue () {
+    nameInput.value = name.textContent;
+    subtitleInput.value = subtitle.textContent;
+}
+function setValue () {
+    name.textContent = nameInput.value;
+    subtitle.textContent = subtitleInput.value;
+}
+
+function togglePopup () {
+    popup.classList.toggle('popup_opened');
+}
+function handleFormSubmit (evt) {
+    evt.preventDefault();
+    setValue();
+    togglePopup();
+}
+
+editBtn.addEventListener('click', () => {
+    getValue();
+    togglePopup();
+});
+popupCloseBtn.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    togglePopup();
+});
+
+popupForm.addEventListener('submit', handleFormSubmit);
