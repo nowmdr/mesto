e3b560ea44171a0ed21fd751eeaf024985a09214
Jauhien Lipko <nowmdr@protonmail.com>
2023-05-24 09:56:04 +0200

    feat: Change way to load images to dynamic

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,30 @@
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ]; 
+
 const popup = document.querySelector('.popup');
 const popupForm = document.querySelector('.popup__form');
 const popupCloseBtn = popup.querySelector('.popup__close-button');
@@ -31,3 +58,18 @@ editBtn.addEventListener('click', () => {
 });
 popupCloseBtn.addEventListener('click', togglePopup);
 popupForm.addEventListener('submit', handleFormSubmit);
+
+const elementTemplate = document.querySelector('#element-template').content; 
+const elementsContainer = document.querySelector('.elements__list');
+
+
+initialCards.forEach((item) => {
+    const copyElement = elementTemplate.querySelector('.element').cloneNode(true);
+    copyElement.querySelector('.element__image').src = item.link;
+    copyElement.querySelector('.element__image').alt = item.name;
+    copyElement.querySelector('.element__title').textContent = item.name;
+    elementsContainer.append(copyElement);
+    console.log(copyElement.querySelector('.element__image').getAttribute('src'),copyElement.querySelector('.element__title'));
+    console.log(elementsContainer);
+
+}) 
